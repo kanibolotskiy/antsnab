b426a060dfcadc710bbba47985fad764f8ab297b
Lapshin Sergey <wslapshin@gmail.com>
2018-08-03 11:22:30 +0300

    for development ready

---
+++ public_html/admin/config.sample.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ant-snab/admin/');
+define('HTTP_CATALOG', 'http://ant-snab/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ant-snab/admin/');
+define('HTTPS_CATALOG', 'http://ant-snab/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/http/ant-snab/public_html/admin/');
+define('DIR_SYSTEM', '/srv/http/ant-snab/public_html/system/');
+define('DIR_IMAGE', '/srv/http/ant-snab/public_html/image/');
+define('DIR_LANGUAGE', '/srv/http/ant-snab/public_html/admin/language/');
+define('DIR_TEMPLATE', '/srv/http/ant-snab/public_html/admin/view/template/');
+define('DIR_CONFIG', '/srv/http/ant-snab/public_html/system/config/');
+define('DIR_CACHE', '/srv/http/ant-snab/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/srv/http/ant-snab/public_html/system/storage/download/');
+define('DIR_LOGS', '/srv/http/ant-snab/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/srv/http/ant-snab/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/srv/http/ant-snab/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/srv/http/ant-snab/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost.localdomain');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'sapsan');
+define('DB_DATABASE', 'antsnab');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ public_html/config.sample.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ant-snab/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://ant-snab/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/http/ant-snab/public_html/catalog/');
+define('DIR_SYSTEM', '/srv/http/ant-snab/public_html/system/');
+define('DIR_IMAGE', '/srv/http/ant-snab/public_html/image/');
+define('DIR_LANGUAGE', '/srv/http/ant-snab/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/srv/http/ant-snab/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/srv/http/ant-snab/public_html/system/config/');
+define('DIR_CACHE', '/srv/http/ant-snab/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/srv/http/ant-snab/public_html/system/storage/download/');
+define('DIR_LOGS', '/srv/http/ant-snab/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/srv/http/ant-snab/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/srv/http/ant-snab/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost.localdomain');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'sapsan');
+define('DB_DATABASE', 'antsnab');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
