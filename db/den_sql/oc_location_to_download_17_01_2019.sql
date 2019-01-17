96321e5771ccf585b71cea7572c0cf95407b48bd
Denis Kanibolotskiy <kanibolotskiy@gmail.com>
2019-01-17 02:23:00 +0200

    download sql

---
+++ db/den_sql/oc_location_to_download_17_01_2019.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 17 2019 г., 03:21
+-- Версия сервера: 5.6.38
+-- Версия PHP: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `antsnab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `oc_location_to_download`
+--
+
+CREATE TABLE `oc_location_to_download` (
+  `location_id` int(11) NOT NULL,
+  `download_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `oc_location_to_download`
+--
+ALTER TABLE `oc_location_to_download`
+  ADD PRIMARY KEY (`download_id`,`location_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
