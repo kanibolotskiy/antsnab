a18d17a9aa5f4a689209d1488c5620d0be50a694
kanibolotskiy <kanibolotskiy@gmail.com>
2019-11-20 14:13:47 +0300

    fix 20.11.2019(1)

--- public_html/app/Override/Controller/Site/Product/SearchController.php
+++ public_html/app/Override/Controller/Site/Product/SearchController.php
@@ -343,8 +343,7 @@ class SearchController extends \Controller
 				$page_str="&page=".($this->request->get['page']-1);
 			}
 			
-			$url_search="https://catalogapi.site.yandex.net/v1.0?apikey=4a4f0a45-0b05-437e-b0dc-5edca1573563&text=".urlencode($this->request->get['text'])."&searchid=2365979&per_page=9".$page_str;
-			//$url_search="https://catalogapi.site.yandex.net/v1.0?apikey=4a4f0a45-0b05-437e-b0dc-5edca1573563&text=".urlencode($this->request->get['text'])."&searchid=2365979&page=3";
+			$url_search="https://catalogapi.site.yandex.net/v1.0?apikey=876ae57f-736c-456f-b961-7cae3d9d8942&text=".urlencode($this->request->get['text'])."&searchid=2367774&per_page=9".$page_str;
 
 			$products_search=file_get_contents($url_search);
 			$products_search_arr=json_decode($products_search,TRUE);
