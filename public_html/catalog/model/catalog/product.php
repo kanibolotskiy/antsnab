48a9ccc11c6d47d000646c4201fcd91cb2a827ed
kanibolotskiy <kanibolotskiy@gmail.com>
2020-01-12 03:41:32 +0300

    fix test(2)

--- public_html/catalog/model/catalog/product.php
+++ public_html/catalog/model/catalog/product.php
@@ -348,8 +348,8 @@ class ModelCatalogProduct extends Model {
 
 			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 		}
-		echo "!".$sql."!";
-		
+		//echo "!".$sql."!";
+
 		$product_data = array();
 
 		$query = $this->db->query($sql);
