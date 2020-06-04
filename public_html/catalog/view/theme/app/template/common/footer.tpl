b66074b75ec5b5646cbc5092508dc23d548b318e
kanibolotskiy <kanibolotskiy@gmail.com>
2020-06-04 04:48:50 +0300

    fix 04.06.2020(2)

--- public_html/catalog/view/theme/app/template/common/footer.tpl
+++ public_html/catalog/view/theme/app/template/common/footer.tpl
@@ -122,7 +122,7 @@
         </div>
     </div>
 </footer>
-<script src="/catalog/view/theme/app/bundle/app2.js?v2.30"></script>
+<script src="/catalog/view/theme/app/bundle/app2.js?v2.31"></script>
 <!--<script src="https://www.google.com/recaptcha/api.js"></script>-->
 
     <!-- @task1 Счетчики, виджеты (comagic) -->
--- src/scripts/yamap.js
+++ src/scripts/yamap.js
@@ -1,17 +1,16 @@
 /** @task этот гребаный яндекс тормозит всю карточку и не работает основной функционал */
+/*
 document.addEventListener("DOMContentLoaded", function () {
+  
     if( undefined !== window.ymaps ) {
         ymaps.ready(init);
     }
-    
     function init() {
-        
         $('.map-container').each(function(index){
             var latitude = $(this).attr('data-latitude'),
                 longitude = $(this).attr('data-longitude'),
                 name = $(this).attr('name'),
                 id = $(this).attr('id');
-
             var map = new ymaps.Map(id, {
                 center: [latitude, longitude],
                 zoom: 15
@@ -21,10 +20,10 @@ document.addEventListener("DOMContentLoaded", function () {
                     {hintContent: name}
             );
             map.geoObjects.add(myPlacemark);
-        });
-        
+        });  
     }
 });
+*/
 $(document).ready(function(){
     $(".map_tab").click(function(e){
         e.preventDefault();
