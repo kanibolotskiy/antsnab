3f9a82eafdebf9b543a0a210b0cb6c3ec19aa1e3
kanibolotskiy <kanibolotskiy@gmail.com>
2020-09-23 08:43:12 +0300

    fixpack 23.09.2020(1)

--- public_html/app/Override/Controller/Site/Extension/Module/SlideshowController.php
+++ public_html/app/Override/Controller/Site/Extension/Module/SlideshowController.php
@@ -42,14 +42,15 @@ class SlideshowController extends \Controller
 			}
 		}
 		$activeAcciasDiscount=$this->model_catalog_sale->activeAcciaDiscount();
+		//print_r($activeAcciasDiscount);
         if($activeAcciasDiscount){
-            if($activeAcciasDiscount["banner"]){
+            if($activeAcciasDiscount["image"]){
                 //$banners[]=array(
 				$data['banners'][]=array(
                     'title' => "Акция",
                     'link'  => "sales/".$activeAcciasDiscount["keyword"],
-					'image' => $this->model_tool_image->cropsize($activeAcciasDiscount["banner"], 1100,540),
-					'descr'=>'Товар дня'
+					'image' => $this->model_tool_image->cropsize($activeAcciasDiscount["image"], 1100,540),
+					'descr'=>$activeAcciasDiscount['shorttext'] //'Товар дня'
                 );
             }
         }
